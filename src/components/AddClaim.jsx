5612d32cdab84bc6b31767771f918635c16f236b
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2023-08-14 22:39:33 -0300

    Upload new post and image

--- src/components/AddClaim.jsx
+++ src/components/AddClaim.jsx
@@ -6,78 +6,109 @@ import {
 	DropdownMenu,
 	DropdownItem,
 	Textarea,
-	Image
 } from "@nextui-org/react"
 import { useFormik } from "formik"
 import * as Yup from "yup"
+import { collection, addDoc } from "firebase/firestore"
+import { getStorage, ref, uploadBytes } from "firebase/storage"
+import { v4 as uuidv4 } from "uuid"
+import { db } from "../firebase.config"
+import { useAuth } from "../hooks/useAuth"
+import { Navbar } from "./Navbar"
 
 export default function AddClaim() {
+	const storage = getStorage()
+	const { user } = useAuth()
 	const formik = useFormik({
 		initialValues: {
 			title: "",
 			content: "",
 			category: "",
-			media: null
+			media: null,
 		},
 		onSubmit: async (values, { setSubmitting }) => {
-			console.log(JSON.stringify(values, null, 2))
-			setSubmitting(false)
+			const { title, category, content, media } = values
+			const id = uuidv4()
 
+			try {
+				const storageRef = ref(storage, `posts/${id}`)
+				uploadBytes(storageRef, media)
+			} catch (error) {
+				console.error(error)
+				throw new Error("Error subiendo la imagen al servidor.")
+			}
+
+			try {
+				await addDoc(collection(db, id), {
+					title,
+					category,
+					content,
+					owner: user.uid,
+					status: "sin revisar",
+					media: [id],
+				})
+			} catch (error) {
+				console.error(error)
+				throw new Error("Error cargando los datos del post.")
+			}
+			resetForm()
+			setSubmitting(false)
 		},
 		validationSchema: Yup.object({
 			title: Yup.string().required("Por favor ingrese un título"),
 			category: Yup.string().required("Por favor seleccione una categoria."),
 		}),
 	})
-	const { values, touched, errors, isSubmitting, setFieldValue } = formik
+
+	const {
+		values,
+		touched,
+		errors,
+		isSubmitting,
+		setFieldValue,
+		resetForm,
+		handleSubmit,
+		getFieldProps,
+	} = formik
 
 	return (
-		<div>
-			<main className="register_form w-full min-h-screen bg-gradient-to-b from-[#FFD73A] from-10% to-50% flex flex-col items-center gap-4">
+		<>
+			<main className="w-full min-h-screen bg-gradient-to-b from-[#FFD73A] from-10% to-50% flex flex-col items-center gap-4">
 				<div className="flex justify-center items-end grow">
-					<img
-						src="/chicken.svg"
-						alt="Logo de la app"
-						width={50}
-					/>
+					<img src="/chicken.svg" alt="Logo de la app" width={50} />
 				</div>
-				<section className="min-h-[40vh] w-8/12 max-w-xs pb-8">
-					<form
-						onSubmit={formik.handleSubmit}
-						className="register-form flex flex-col gap-4 p-4 ">
+				<section className="min-h-[40vh] w-full max-w-xs pb-12">
+					<form onSubmit={handleSubmit} className="flex flex-col gap-4 p-4 ">
 						<div className="flex flex-col gap-1">
 							<Dropdown type="listbox">
 								<DropdownTrigger>
 									<Button variant="bordered">
-										{formik.values.category === ""
-											? "Seleccione una categoría"
-											: formik.values.category}
+										{values.category === "" ? "Seleccione una categoría" : values.category}
 									</Button>
 								</DropdownTrigger>
 								<DropdownMenu
-									
 									name="category"
 									aria-label="Categoría"
 									selectionMode="single"
 									disallowEmptySelection
-									selectedKeys={formik.values.category}
-									onAction={(value) => formik.setFieldValue("category", value)}>
+									selectedKeys={values.category}
+									onAction={(value) => setFieldValue("category", value)}>
 									<DropdownItem key="Reclamos">Reclamos</DropdownItem>
 									<DropdownItem key="Sugerencias">Sugerencias</DropdownItem>
 									<DropdownItem key="Denuncias">Denuncias</DropdownItem>
 								</DropdownMenu>
 							</Dropdown>
-							{formik.touched.category && formik.errors.category ? (
-								<div className="text-tiny text-danger">{formik.errors.category}</div>
+							{touched.category && errors.category ? (
+								<div className="text-tiny text-danger">{errors.category}</div>
 							) : null}
 						</div>
-						
+
 						<Input
 							name="title"
 							label="Título"
 							isRequired
 							variant="underlined"
-							{...formik.getFieldProps("title")}
+							{...getFieldProps("title")}
 							validationState={touched.title && errors.title ? "invalid" : "valid"}
 							errorMessage={touched.title && errors.title ? errors.title : ""}
 						/>
@@ -87,20 +118,23 @@ export default function AddClaim() {
 							labelPlacement="inside"
 							variant="underlined"
 							placeholder="Ingrese una descripción"
-							{...formik.getFieldProps("content")}
+							{...getFieldProps("content")}
 						/>
 
 						<input
 							type="file"
 							name="media"
 							id="media"
-							capture
-							onChange={e => setFieldValue("media", e.target.files[0])}
+							onChange={(e) => setFieldValue("media", e.target.files[0])}
 							accept="image/*"
 							className="shadow-sm text-sm px-3 rounded-medium transition-background motion-reduce:transition-none outline-none !duration-150 focus-visible:ring-2 focus-visible:ring-focus focus-visible:ring-offset-2 focus-visible:ring-offset-background py-2"
 						/>
-						{values.media ? <Image src={URL.createObjectURL(values.media)} /> : ""}
-						
+						{values.media ? (
+							<img className="rounded-lg block" src={URL.createObjectURL(values.media)} />
+						) : (
+							""
+						)}
+
 						<Button
 							type="submit"
 							className="bg-gold"
@@ -131,6 +165,7 @@ export default function AddClaim() {
 					</form>
 				</section>
 			</main>
-		</div>
+			<Navbar />
+		</>
 	)
 }
