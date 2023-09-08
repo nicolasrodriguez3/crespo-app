8279988d45059e821d7d9b8640a04a688fa4bf44
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2023-09-08 19:08:49 -0300

    Add .env

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-	apiKey: "AIzaSyDbi5WBIX3lGwd5qUyCn4S_SfLJkQOxHrA",
-	authDomain: "crespo-app.firebaseapp.com",
-	projectId: "crespo-app",
-	storageBucket: "crespo-app.appspot.com",
-	messagingSenderId: "158683685873",
-	appId: "1:158683685873:web:5ebb6cd5895f5bada19a50",
-	measurementId: "G-GVF57GBTXS",
+	apiKey: import.meta.env.apiKey,
+	authDomain: import.meta.env.authDomain,
+	projectId: import.meta.env.projectId,
+	storageBucket: import.meta.env.storageBucket,
+	messagingSenderId: import.meta.env.messagingSenderId,
+	appId: import.meta.env.appId,
+	measurementId: import.meta.env.measurementId,
 }
 
 // Initialize Firebase
