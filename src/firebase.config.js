10ac3e08f4d17bd23f5a18deebd3d4d91cb19b6c
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2023-09-08 19:20:45 -0300

    Fix error .env variables

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -6,14 +6,14 @@ import { getFirestore } from "firebase/firestore"
 import { getStorage } from "firebase/storage"
 
 const firebaseConfig = {
-	apiKey: import.meta.env.apiKey,
-	authDomain: import.meta.env.authDomain,
-	projectId: import.meta.env.projectId,
-	storageBucket: import.meta.env.storageBucket,
-	messagingSenderId: import.meta.env.messagingSenderId,
-	appId: import.meta.env.appId,
-	measurementId: import.meta.env.measurementId,
-}
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
+  measurementId: import.meta.env.VITE_MEASUREMENT_ID
+};
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig)
