7595fef78c9170a21a75b8285f1b034d615b924e
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2023-09-25 23:05:04 -0300

    Add mock for API: Reclamos

---
+++ src/mocks/reclamos.mock.js
@@ -0,0 +1,57 @@
+export const reclamo = [
+  {
+    "id": "01",
+    "coordinadaX": "string",
+    "coordinadaY": "string",
+    "descripcion": "string",
+    "altura": "string",
+    "imagen": {
+      "id": "string",
+      "path": "string",
+      "nombre": "string"
+    },
+    "seguimiento": {
+      "id": "string",
+      "estados": [
+        {
+          "id": "string",
+          "estado": "string",
+          "descripcion": "string"
+        }
+      ]
+    },
+    "calle": {
+      "id": "string",
+      "calle": "string"
+    },
+    "barrio": {
+      "id": "string",
+      "barrio": "string"
+    },
+    "interseccion": {
+      "id": "string",
+      "calle": "string"
+    },
+    "entreCalle1": {
+      "id": "string",
+      "calle": "string"
+    },
+    "entreCalle2": {
+      "id": "string",
+      "calle": "string"
+    },
+    "persona": {
+      "id": "string",
+      "nombre": "string",
+      "dni": "string",
+      "direccion": "string",
+      "telefono": "string",
+      "username": "string",
+      "roles": []
+    },
+    "tipoReclamo": {
+      "id": "string",
+      "tipo": "string"
+    }
+  }
+]
