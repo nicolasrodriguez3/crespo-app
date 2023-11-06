b31ea276c496e83e544859996daabd6f499b4933
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2023-11-06 11:49:42 -0300

    Add manifest and icons to vite.config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,53 @@ import { VitePWA } from "vite-plugin-pwa"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA({ registerType: "autoUpdate" })],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      manifestFilename: "manifest.webmanifest",
+      includeAssets: [
+        "favicon.ico",
+        "robots.txt",
+        "apple-touch-icon.png",
+        "android-chrome-192x192.png",
+        "android-chrome-512x512.png",
+        "maskable_icon_x512.png",
+      ],
+      manifest: {
+        name: "Somos Crespo",
+        short_name: "SomosCrespo",
+        description:
+          "Aplicación para cargar y dar seguimiento a reclamos de los ciudadanos",
+        display: "standalone",
+        orientation: "portrait",
+        lang: "es-AR",
+        start_url: "/",
+        theme_color: "#ffcc00",
+        icons: [
+          {
+            src: "apple-touch-icon.png",
+            sizes: "180x180",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "maskable_icon_x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 })
