930de73bfc6bade3523599efd9bbca4eb0e2affa
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2023-11-10 16:08:34 -0300

    Add new permissions to PERMISSIONS.js

--- src/constants/PERMISSIONS.js
+++ src/constants/PERMISSIONS.js
@@ -2,4 +2,8 @@ export const PERMISSIONS = {
   // ver eliminados
   calle: "CAPATAZ",
   usuario: "JEFE",
+  "tipo de reclamo": "CAPATAZ",
+  reclamo: "CAPATAZ",
+  barrio: "CAPATAZ",
+  area: "CAPATAZ",
 }
