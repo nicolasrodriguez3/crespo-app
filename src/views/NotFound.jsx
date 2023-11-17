e5824fb21d0513a7558e21686330f7674b147ba8
Nicolas Rodriguez <nicolasrodriguez@outlook.com.ar>
2023-11-17 12:19:32 -0300

    Update NotFound component with new image and error message

--- src/views/NotFound.jsx
+++ src/views/NotFound.jsx
@@ -1,21 +1,30 @@
 import { useRouteError } from "react-router-dom"
-import chicken from "../assets/icons/roast-chicken.svg"
+import chicken from "../assets/imgs/sad-chicken.png"
 
 export function NotFound() {
   const { status, statusText } = useRouteError()
 
   return (
-    <main className="flex min-h-screen w-full flex-col items-center gap-4 bg-gradient-to-b from-[#FFD73A] from-10% to-50%">
+    <main className="flex min-h-screen w-full flex-col items-center gap-2 bg-gradient-to-b from-[#FFD73A] from-10% to-50%">
       <div className="flex grow items-end justify-center">
         <img
           src={chicken}
-          alt="Ícono de un pollo cocido"
-          width={100}
+          alt="Ícono de una gallina triste"
+          width={120}
         />
       </div>
-      <section className="flex flex-col p-4 pb-28 text-center text-xl">
-        <p>Error {status}</p>
+      <section className="flex flex-col p-3 pb-40 text-center text-xl">
+        <p>Ocurrió un error {status}</p>
         <p className="italic">{statusText}</p>
+        <p className="text-base">
+          Intente recargar la página o{" "}
+          <a
+            href="/"
+            className="underline underline-offset-2 "
+          >
+            ir a la pagina de inicio
+          </a>
+        </p>
       </section>
     </main>
   )
